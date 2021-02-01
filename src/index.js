9c3d63a5bce0922b8cc3a90eb3d15d3d63c44935
willie117 trombone <ilia.yasau@gmail.com>
2021-02-01 00:36:24 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,115 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import grabity from 'grabity';
-import { useBottomScrollListener } from 'react-bottom-scroll-listener';
-import {
-  GoogleFonts, Container, Title, Filter, List, ListItem,
-  ImageContainer, BaseImage, LinkContainer, Link, Button
-} from './styles';
-
-function Image({ url, alt }) {
-  const [image, setImage] = React.useState('');
-
-  React.useEffect(() => {
-    async function getImage() {
-      let response = await grabity.grabIt(url);
-      if (response && response.image) {
-        const { image } = response;
-        setImage(image);
-      } else {
-        setImage('https://icons.iconarchive.com/icons/thehoth/seo/256/seo-article-icon.png');
-      }
-    }
-    getImage();
-  }, [url]);
-
-  return (
-    <BaseImage src={image} alt={alt} />
-  );
-}
-
-function HackerNewsPosts({ posts, filter }) {
-  if (posts.length === 0) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <List>
-      {posts.map(post => {
-        if (post && post.title && post.url) {
-          const lowerCaseTitle = post.title.toLowerCase();
-          if (!lowerCaseTitle.includes(filter)) {
-            return null;
-          }
-          return (
-            <ListItem key={post.id}>
-              <ImageContainer>
-                <Image url={post.url} alt={post.title} />
-              </ImageContainer>
-              <LinkContainer>
-                <Link href={post.url}>{post.title}</Link>
-              </LinkContainer>
-            </ListItem>
-          )
-        }
-        return null;
-      })}
-    </List>
-  );
-}
+import Weather from './components/weather';
+import './styles/index.css';
 
 function App() {
-  const [posts, setPosts] = React.useState([]);
-  const [filter, setFilter] = React.useState('');
-  const [quantity, setQuantity] = React.useState(30);
-
-  React.useEffect(() => {
-    async function getTopStories() {
-      const url = "https://hacker-news.firebaseio.com/v0/newstories.json";
-      try {
-        const response = await fetch(url);
-        if (response.ok === false) {
-          throw new Error("Response Error:" + response.text);
-        }
-        const json = await response.json();
-        const promises = json
-          .slice(0, quantity)
-          .map(id =>
-            fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(
-              response => response.json()
-            )
-          );
-        const result = await Promise.all(promises);
-        setPosts(result);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-    getTopStories();
-  }, [quantity]);
-
-  function loadMorePosts() {
-    let updatedQuantity = quantity + 30;
-    setQuantity(updatedQuantity);
-  }
-
-  useBottomScrollListener(loadMorePosts);
-
-  function filterPosts(event) {
-    const value = event.target.value.toLowerCase();
-    setFilter(value);
-  }
-
   return (
-    <div className="App">
-      <GoogleFonts />
-      <Container>
-        <Title>HackerNews</Title>
-        <Filter placeholder="Filter" onChange={e => filterPosts(e)} />
-        <HackerNewsPosts posts={posts} filter={filter} />
-        <Button onClick={() => loadMorePosts()}>
-          Load more posts
-        </Button>
-      </Container>
+    <div className="app">
+      <Weather />
     </div>
   );
 }
